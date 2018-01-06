0922e0f21cac66e18df4ef3fb9833e1f04fc4c4e
Jasper den Hertog <jasper@plainspace.com>
2018-01-06 16:45:47 +0100

    cleanup unused imports + println

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 
 extern crate dns_lookup;
 extern crate hex_slice;
-extern crate ipaddress;
 extern crate ipnetwork;
 extern crate pnet;
 extern crate rand;
@@ -13,8 +12,8 @@ use std::iter::Iterator;
 
 use std::io::{self, Error, ErrorKind};
 
-use std::net::{IpAddr, Ipv4Addr, SocketAddrV4};
-use std::net::{Ipv6Addr, SocketAddr, SocketAddrV6, ToSocketAddrs};
+use std::net::{IpAddr, SocketAddrV4};
+use std::net::{SocketAddr, SocketAddrV6, ToSocketAddrs};
 use ipnetwork::IpNetwork;
 
 use time::{Duration, SteadyTime};
@@ -22,7 +21,6 @@ use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 use pnet::packet::icmp::{IcmpPacket, IcmpTypes};
 use pnet::packet::icmpv6::{Icmpv6Packet, Icmpv6Types};
 use pnet::packet::icmp::echo_request::MutableEchoRequestPacket;
-//use pnet::packet::icmpv6::echo_request::MutableEchoRequestPacket as MutableEchoRequestV6Packet;
 use pnet::packet::icmp::echo_reply::EchoReplyPacket;
 use pnet::packet::icmp::time_exceeded::TimeExceededPacket;
 use pnet::packet::Packet;
@@ -30,14 +28,13 @@ use pnet::packet::icmp::checksum;
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ipv6::Ipv6Packet;
 use pnet::packet::icmpv6::MutableIcmpv6Packet;
-use pnet::datalink::{interfaces, NetworkInterface};
+use pnet::datalink::NetworkInterface;
 
-use ipaddress::IPAddress;
 use dns_lookup::lookup_addr;
-use hex_slice::AsHex;
+// only used for debig printing packets
+//use hex_slice::AsHex;
 
-//const MAX_PACKET_SIZE: usize = 4096 + 128;
-const MAX_PACKET_SIZE: usize = 128;
+const MAX_PACKET_SIZE: usize = 4096 + 128;
 const ICMP_HEADER_LEN: usize = 8;
 const SRC_BASE_PORT: u16 = 20480;
 
@@ -58,9 +55,6 @@ pub fn make_icmp6_packet(payload: &[u8]) -> Icmpv6Packet {
 pub struct TraceResult {
     dest_addr: SocketAddr,
     af: AddressFamily,
-    //socket: Socket,
-    //protocol: Protocol,
-    //echo_request_packet: IcmpEchoRequest<'a>,
     ttl: u32,
     ident: u16,
     seq_num: u16,
@@ -162,44 +156,6 @@ impl TraceResult {
         }
     }
 
-    fn get_sock_addr(&self) -> SockAddr {
-        //let interface_has_ips_match = |iface: &NetworkInterface| iface.ips.len() > 0;
-        let interfaces = pnet::datalink::interfaces();
-        let interface = interfaces
-            .into_iter()
-            .filter(|iface: &NetworkInterface| iface.ips.len() > 0)
-            .flat_map(|i| i.ips)
-            .filter(|addr: &IpNetwork| addr.is_ipv6() && addr.ip().is_global())
-            .map(|a| a.ip());
-        //.iter()
-        //.filter(|iface: &NetworkInterface| iface.name == "en0" )
-        //.next()
-        //.unwrap();
-        //println!("ips: {:?}", interface);
-        for intf in interface {
-            println!("if: {:?}", intf);
-        }
-        //     .ips
-        //     .into_iter()
-        //     .filter(
-        //        |addr: &IpNetwork| addr.is_ipv6()
-        //     )
-        // );
-
-        match self.af {
-            AddressFamily::V4 => <SockAddr>::from(SocketAddrV4::new(
-                Ipv4Addr::new(192, 168, 178, 100),
-                SRC_BASE_PORT,
-            )),
-            AddressFamily::V6 => <SockAddr>::from(SocketAddrV6::new(
-                Ipv6Addr::new(0x2001, 0x470, 0x1f15, 0xf8d, 0xa65e, 0x60ff, 0xfec2, 0xc373),
-                SRC_BASE_PORT,
-                0,
-                0x0,
-            )),
-        }
-    }
-
     fn find_next_hop(&mut self) -> io::Result<TraceHop> {
         let src = self.get_sock_addr2();
         println!("src: {:?}", src);
@@ -223,37 +179,31 @@ impl TraceResult {
 
             // After deadline passes, restart the loop to advance the TTL and resend.
             while SteadyTime::now() < start_time + self.timeout {
-                let (packet_len, sender);
+                //let packet_len;
+                let sender;
                 let mut buf_in = vec![0; MAX_PACKET_SIZE];
                 match socket.recv_from(buf_in.as_mut_slice()) {
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => continue,
                     Err(e) => return Err(e),
                     Ok((len, s)) => {
-                        packet_len = len;
+                        //packet_len = len;
                         sender = s;
                         println!("length: {}", len);
                     }
                 }
 
                 // The IP packet that wraps the incoming ICMP message.
-                //let ip_packet_in = Ipv6Packet::new(&buf_in).unwrap().payload().to_owned();
                 let packet_in = self.unwrap_payload_ip_packet_in(&buf_in);
 
-                //println!("{:?}", ip_packet_in);
-                //println!("{:?}", buf_in);
-                // The ICMP packet hopefully inside the payload of the IP packet
-                //let icmp_packet_in = Icmpv6Packet::new(&buf_in).unwrap();
-                //println!("payload#1: {:?}", icmp_packet_in);
                 match packet_in {
                     IcmpPacketIn::V6(icmp_packet_in) => match icmp_packet_in.get_icmpv6_type() {
                         Icmpv6Types::EchoReply => {
-                            println!("Echo reply in: {:?}", icmp_packet_in);
-                            println!("icmp payload: {:02x}", icmp_packet_in.payload().as_hex());
+                            //println!("Echo reply in: {:?}", icmp_packet_in);
+                            //println!("icmp payload: {:02x}", icmp_packet_in.payload().as_hex());
                             let icmp_echo_reply = &icmp_packet_in;
                             if icmp_echo_reply.get_identifier() == self.ident
                                 && icmp_echo_reply.get_sequence_number() == self.seq_num
                             {
-                                println!("echo reply; end now");
                                 let host = SocketAddr::V6(sender.as_inet6().unwrap());
                                 let hop = TraceHop {
                                     ttl: self.ttl,
@@ -269,7 +219,7 @@ impl TraceResult {
                             println!("dest unreachable; end now")
                         }
                         Icmpv6Types::TimeExceeded => {
-                            println!("time exceeded: {:02x}", icmp_packet_in.payload().as_hex());
+                            //println!("time exceeded: {:02x}", icmp_packet_in.payload().as_hex());
                             // `Time Exceeded` packages do not have a identifier or sequence number
                             // They do return up to 576 bytes of the original IP packet
                             // So that's where we identify the packet to belong to this `packet_out`.
@@ -279,7 +229,7 @@ impl TraceResult {
                             }
                             let wrapped_ip_packet =
                                 Ipv6Packet::new(&icmp_packet_in.payload()).unwrap();
-                            println!("unwrap ip: {:?}", wrapped_ip_packet);
+                            //println!("unwrap ip: {:?}", wrapped_ip_packet);
                             // We don't have any ICMP data right now
                             // So we're only using the last 4 bytes in the payload to compare.
                             if wrapped_ip_packet.payload()[4..8] == packet_out[4..8] {
@@ -293,9 +243,7 @@ impl TraceResult {
                                 return Ok(hop);
                             }
                         }
-                        _ => (
-                        //println!("incoming packet {:?}", icmp_packet_in)
-                    ),
+                        _ => (),
                     },
                     IcmpPacketIn::V4(ip_packet_in) => {
                         let ip_payload = ip_packet_in.payload();
@@ -348,7 +296,7 @@ impl TraceResult {
                                     return Ok(hop);
                                 }
                             }
-                            _ => (println!("incoming packet {:?}", icmp_packet_in)),
+                            _ => (println!("incoming time exceeded unknown {:?}", icmp_packet_in)),
                         }
                     }
                 }
